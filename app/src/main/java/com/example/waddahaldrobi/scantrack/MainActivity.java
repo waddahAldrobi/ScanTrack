c2bc45182222fa017b1a3d753665f4b362b36101
waddahAldrobi <aldrobiwaddah@gmail.com>
2018-07-19 10:42:35 -0400

    Fixed permissions

--- app/src/main/java/com/example/waddahaldrobi/scantrack/MainActivity.java
+++ app/src/main/java/com/example/waddahaldrobi/scantrack/MainActivity.java
@@ -1,21 +1,36 @@
 package com.example.waddahaldrobi.scantrack;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.zxing.Result;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private static final int ZXING_CAMERA_PERMISSION = 1;
+    private Class<?> mClss;
     private ZXingScannerView zXingScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermissions();
+
+
     }
 
     public void scan(View view){
@@ -35,7 +50,44 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
     @Override
     public void handleResult(Result result) {
         Toast.makeText(getApplicationContext(),result.getText(),Toast.LENGTH_SHORT).show();
-        zXingScannerView.resumeCameraPreview(this);
+//        zXingScannerView.resumeCameraPreview(this);
+        Log.d("Result: ", "handleResult: " + result.getText().toString());
 
     }
+
+    //Permissions
+    String[] permissions = new String[]{
+            Manifest.permission.CAMERA
+    };
+
+    private boolean checkPermissions() {
+        int result;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String p : permissions) {
+            result = ContextCompat.checkSelfPermission(this, p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), 100);
+            return false;
+        }
+        return true;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == 100) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Please grant camera permission to use the QR Scanner", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+    }
+
+
+
 }
